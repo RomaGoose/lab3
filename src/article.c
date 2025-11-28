d7a528d2c1c967a3d070a78ed227f0ac396c7aa2
roma <roma.katkov73@gmail.com>
2025-11-28 21:23:50 +0300

    refactor: change article and cmp to normal ones

--- src/article.c
+++ src/article.c
@@ -1,16 +1,35 @@
 #include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 typedef struct _Article {
-    int n; 
-    char name[20];
+    char article_name[120];
+    char author_surname[24];
+    char author_initials[8];
+    char magazine[50];
+    uint16_t year;
+    uint16_t mag_vol;
+    uint16_t pages;
+    uint16_t citations;
+    uint8_t in_RSCI;
 } Article;
 
 int cmp(void* left, void* right){
     Article* l = (Article*)left;
     Article* r = (Article*)right;
 
-    if(l->n == r->n){
-        if(strcmp(l->name,r->name) == 0){
-            return 0;
-        }return strcmp(l->name,r->name);
-    }return l->n - r->n;
+    
+    if(l->citations == r->citations){
+        if(l->in_RSCI == r->in_RSCI){
+            if(l->year == r->year){
+                if(l->pages == r->pages){
+                    if(strcmp(l->article_name, r->article_name)){
+                        if(strcmp(l->author_surname,r->author_surname) == 0){
+                            return 0;
+                        }return strcmp(l->author_surname,r->author_surname);
+                    }return l->article_name - r->article_name;
+                }return l->pages - r->pages;
+            }return l->year - r->year;
+        }return l->in_RSCI - r->in_RSCI;
+    }return l->citations - r->citations;
 }
